e694a0d13d6e4b8bdd842e5afb009df36e4586ba
Bastien <bastienrobert@outlook.com>
2018-03-20 12:54:37 +0100

    Add new icons and create credits and game controller

---
+++ mastermind/CreditsController.swift
@@ -0,0 +1,25 @@
+//
+//  CreditsController.swift
+//  mastermind
+//
+//  Created by Bastien Robert on 20/03/2018.
+//  Copyright © 2018 Bastien Robert. All rights reserved.
+//
+import UIKit
+
+class CreditsController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }   
+    
+}
+
+
+
---
+++ mastermind/GameController.swift
@@ -0,0 +1,28 @@
+//
+//  GameController.swift
+//  mastermind
+//
+//  Created by Bastien Robert on 20/03/2018.
+//  Copyright © 2018 Bastien Robert. All rights reserved.
+//
+
+import UIKit
+
+class GameController: ViewController {
+    
+    @IBOutlet weak var newButton: UIButton!
+    @IBOutlet weak var labelCounter: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+
