adf328c3020206f9d167e2a79d415450538ed3ab
Bastien <bastienrobert@outlook.com>
2018-03-20 16:57:22 +0100

    Add counter to game model

--- mastermind/Game.swift
+++ mastermind/Game.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class Game {
+    
+    var timer : Int = 0
+    init() {
+        self.startTimer()
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.setTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func setTimer() {
+        timer += 1
+        let notification = Notification(name: Notification.Name("timerUpdated"))
+        NotificationCenter.default.post(notification)
+    }
+    
+    func getTimer() -> String {
+        let m : Int = timer / 60
+        let s : Int = timer % 60
+        return (String(format: "%02d:%02d", m, s))
+    }
+    
+}
--- mastermind/GameController.swift
+++ mastermind/GameController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class GameController: ViewController {
     
+    var myGame = Game()
+    
     @IBOutlet weak var newButton: UIButton!
     @IBOutlet weak var labelCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(timer), name: Notification.Name(rawValue: "timerUpdated"), object: nil)
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func timer() {
+        self.labelCounter.text = myGame.getTimer()
     }
     
     override func didReceiveMemoryWarning() {
