14e3100cae924c98ca04b6efb5e9fca33dc43c7a
Bastien <bastienrobert@outlook.com>
2018-03-21 16:44:00 +0100

    Colors are working in the bottom, and are now buttons

--- mastermind/Game.swift
+++ mastermind/Game.swift
@@ -7,12 +7,79 @@
 //
 
 import Foundation
+import UIKit
 
 class Game {
     
     var timer : Int = 0
+    var solution : Array<Any> = []
+    
+    enum Color: Int {
+        case strawberry = 0, banana = 1, perry = 2, orange = 3, peach = 4, grape = 5
+        
+        func label() -> [String: Any] {
+            switch self {
+            case .strawberry:
+                return [
+                    "icon": "🍓",
+                    "backgroundColor": UIColor(red:0.98, green:0.92, blue:0.92, alpha:1.0),
+                    "borderColor": UIColor(red:0.96, green:0.85, blue:0.84, alpha:1.0).cgColor
+                ]
+            case .banana:
+                return [
+                    "icon": "🍌",
+                    "backgroundColor": UIColor(red:1.00, green:0.99, blue:0.91, alpha:1.0),
+                    "borderColor": UIColor(red:0.99, green:0.98, blue:0.83, alpha:1.0).cgColor
+                ]
+            case .perry:
+                return [
+                    "icon": "🍐",
+                    "backgroundColor": UIColor(red:0.95, green:0.98, blue:0.91, alpha:1.0),
+                    "borderColor": UIColor(red:0.90, green:0.96, blue:0.83, alpha:1.0).cgColor
+                ]
+            case .orange:
+                return [
+                    "icon": "🍊",
+                    "backgroundColor": UIColor(red:1.00, green:0.96, blue:0.91, alpha:1.0),
+                    "borderColor": UIColor(red:0.99, green:0.93, blue:0.84, alpha:1.0).cgColor
+                ]
+            case .peach:
+                return [
+                    "icon": "🍑",
+                    "backgroundColor": UIColor(red:0.98, green:0.95, blue:0.94, alpha:1.0),
+                    "borderColor": UIColor(red:0.97, green:0.91, blue:0.88, alpha:1.0).cgColor
+                ]
+            case .grape:
+                return [
+                    "icon": "🍇",
+                    "backgroundColor": UIColor(red:0.98, green:0.95, blue:0.98, alpha:1.0),
+                    "borderColor": UIColor(red:0.96, green:0.91, blue:0.96, alpha:1.0).cgColor
+                ]
+            }
+        }
+        
+        static var count: Int {
+            get {
+                var max: Int = 0
+                while let _ = Color(rawValue: max) { max += 1 }
+                return max
+            }
+        }
+        
+        static var values: [Color] {
+            get {
+                var response = [Color]()
+                for i in 0..<Color.count {
+                    response.append(Color(rawValue: i)!)
+                }
+                return response
+            }
+        }
+    }
+    
     init() {
         self.startTimer()
+        self.solution = self.createCombination()
     }
     
     func startTimer() {
@@ -29,6 +96,16 @@ class Game {
         let m : Int = timer / 60
         let s : Int = timer % 60
         return (String(format: "%02d:%02d", m, s))
+    }
+    
+    func createCombination() -> Array<Any> {
+        var combination: Array<Any> = []
+        var random : Int
+        for _ in 0..<4 {
+            random = Int(arc4random_uniform(UInt32(Color.count)))
+            combination.append(Color(rawValue: random)!.label() as Any)
+        }
+        return combination
     }
     
 }
--- mastermind/GameController.swift
+++ mastermind/GameController.swift
@@ -12,13 +12,26 @@ class GameController: ViewController {
     
     var myGame = Game()
     
+    // Buttons and labels
     @IBOutlet weak var newButton: UIButton!
     @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet var fruitButtons: [UIButton]!
+    
+    // Actions vars
+    
+    @IBAction func fruitBtnPressed(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(timer), name: Notification.Name(rawValue: "timerUpdated"), object: nil)
         
-        // Do any additional setup after loading the view, typically from a nib.
+        for button in fruitButtons {
+            let params = Game.Color(rawValue: button.tag)!.label()
+            customizeControls(button: button, params: params)
+        }
     }
     
     @objc func timer() {
@@ -28,6 +41,15 @@ class GameController: ViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func customizeControls(button: UIButton, params: [String : Any]) {
+        button.setTitle(params["icon"] as? String, for: .normal)
+        button.backgroundColor = params["backgroundColor"] as? UIColor
+        button.layer.borderColor = (params["borderColor"] as! CGColor)
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 2
     }
     
 }
